2f27e222e2065ddf9bb4facae6c16a03086ec686
Kyle Kelley <rgbkrk@gmail.com>
2016-09-25 14:16:53 -0700

    Create a basic proxy server.

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const httpProxy = require('http-proxy');
+
+const proxy = httpProxy.createProxyServer();
+
+http.createServer(function(req, res) {
+  proxy.web(req, res, { target: 'https://nteract.github.io' });
+}).listen(80);
+
+
