220ecfb9cf59e5ef09b598bd704b24474b79cb6b
Kyle Kelley <rgbkrk@gmail.com>
2017-03-02 14:03:44 -0800

    bridge express, now, and http proxy

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-const http = require('http');
-const httpProxy = require('http-proxy');
+const httpProxy = require("http-proxy");
+const express = require("express");
+const next = require("next");
 
 const proxy = httpProxy.createProxyServer({ changeOrigin: true });
 
-http.createServer(function(req, res) {
-  proxy.web(req, res, { target: 'https://nteract.github.io' });
-}).listen(80);
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.get("/kernels", (req, res) => {
+    return app.render(req, res, "/kernels", req.query);
+  });
+
+  server.get("*", (req, res) => {
+    return proxy.web(req, res, { target: "https://nteract.github.io" });
+  });
+
+  server.listen(3000, err => {
+    if (err) throw err;
+    console.log("> Ready on http://localhost:3000");
+  });
+});
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Link from "next/link";
+
+export default () => (
+  <ul>
+    <li><Link href="/kernels" as="/kernels"><a>kernels</a></Link></li>
+  </ul>
+);
