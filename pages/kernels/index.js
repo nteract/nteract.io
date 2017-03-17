3e65232e98e9eeff7ccabb37caf4b8bcf818f564
Kyle Kelley <rgbkrk@gmail.com>
2017-03-17 15:56:33 -0700

    make kernels pages all keep the same header

--- pages/kernels/index.js
+++ pages/kernels/index.js
@@ -8,18 +8,7 @@
 
 import Head from "next/head";
 //Sections
-import Layout from "../../components/layout/layout";
-import ContentSection from "../../components/content-section/content-section";
-import PageHeader from './page-header'
 
 import Python from "./python";
 
-
-export default () => (
-    <Layout pageTitle=": The nteract Desktop App">
-        <PageHeader />
-        <ContentSection>
-            <Python />
-        </ContentSection>
-    </Layout>
-);
+export default () => <Python />;
