dd028e5ed4afe4fa12acae3e679e854d268d8cb5
Kyle Kelley <rgbkrk@gmail.com>
2017-04-28 12:42:31 -0700

    Link direct to the blog
    Less domain name resolution, less redirects

--- components/navigation/navigation-left.js
+++ components/navigation/navigation-left.js
@@ -5,7 +5,7 @@ import styles from "./navigation.scss";
 export default () => (
   <div className="nav buttons" role="navigation">
     <style dangerouslySetInnerHTML={{ __html: styles }} />
-    <a href="https://medium.com/nteract" target="_blank" className="nav-item">
+    <a href="https://blog.nteract.io/" target="_blank" className="nav-item">
       Blog
     </a>
     <Link href="/about" as="/about">
