b4d6d04e242567739d5278cdf3e181f166154651
Lukas Geiger <lukas.geiger94@gmail.com>
2017-10-15 01:00:14 +0200

    Use GitHub GraphQL API to get members

--- pages/about.js
+++ pages/about.js
@@ -1,17 +1,27 @@
 // @flow
+import { GraphQLClient } from "graphql-request";
+import { Component } from "react";
+
 import Layout from "../components/layout/layout";
 import {
   ContentSection,
   ContentSectionPane
 } from "../components/content-section/content-section";
-import React from "react";
 import {
   PageHeader,
   PageHeaderLeft,
   PageHeaderRight
 } from "../components/page-header/page-header";
 
-const contributorsData = require("nteract-members");
+type Member = {
+  name: ?string,
+  login: string,
+  websiteUrl: ?string,
+  avatarUrl: string,
+  url: string
+};
+
+type Members = Array<Member>;
 
 const Mission = () => (
   <ContentSection>
@@ -25,46 +35,87 @@ const Mission = () => (
   </ContentSection>
 );
 
-const ContributorsList = contributorsData.map((person, index) => {
-  if (!person.login) {
-    return null;
-  }
-  return (
-    <div key={index} className="person">
-      <div className="person-avatar">
-        <img src={person.avatar_url} />
-      </div>
-      <div className="person-details">
-        <div className="person-name">{person.name || "@" + person.login}</div>
-      </div>
-      <div className="person-social">
-        <div key={index} className="social-item">
-          <a href={person.html_url} target="_blank">
-            <i className="mdi mdi-github-circle" />
+const Contributor = ({ person }: { person: Member }) => (
+  <div className="person">
+    <div className="person-avatar">
+      <img src={person.avatarUrl} />
+    </div>
+    <div className="person-details">
+      <div className="person-name">{person.name || "@" + person.login}</div>
+    </div>
+    <div className="person-social">
+      <div className="social-item">
+        <a href={person.url} target="_blank">
+          <i className="mdi mdi-github-circle" />
+        </a>
+        {person.websiteUrl ? (
+          <a href={person.websiteUrl} target="_blank">
+            <i className="mdi mdi-web" />
           </a>
-          {person.blog ? (
-            <a href={person.blog} target="_blank">
-              <i className="mdi mdi-web" />
-            </a>
-          ) : null}
-        </div>
+        ) : null}
       </div>
     </div>
-  );
-});
-
-const Contributors = () => (
-  <ContentSection>
-    <ContentSectionPane layout="center">
-      <h3>Contributors</h3>
-      <div className="grid">
-        <div className="grid-wrapper">{ContributorsList}</div>
-      </div>
-    </ContentSectionPane>
-  </ContentSection>
+  </div>
 );
 
-export default class AboutPage extends React.Component<void, OSProps, void> {
+const Contributors = ({ members }: { members: ?Members }) => {
+  if (!members || members.length === 0) return null;
+  return (
+    <ContentSection>
+      <ContentSectionPane layout="center">
+        <h3>Contributors</h3>
+        <div className="grid">
+          <div className="grid-wrapper">
+            {members.map(person => (
+              <Contributor person={person} key={person.login} />
+            ))}
+          </div>
+        </div>
+      </ContentSectionPane>
+    </ContentSection>
+  );
+};
+
+export default class AboutPage extends Component<void, OSProps, void> {
+  static async getInitialProps() {
+    const client = new GraphQLClient("https://api.github.com/graphql", {
+      headers: {
+        Authorization: "Bearer 243492ac50267b053ca0d23d73a59f9e46bee565"
+      }
+    });
+
+    const query = `{
+      organization(login: "nteract") {
+        members(first: 100) {
+          totalCount
+            nodes {
+              name
+              login
+              websiteUrl
+              avatarUrl
+              url
+            }
+        }
+      }
+    }`;
+
+    let members;
+    try {
+      const data = await client.request(query);
+      members = data.organization.members.nodes;
+      if (data.organization.members.totalCount > 100) {
+        console.error(
+          "100+ members in the organization. That's too much for one GraphQL call."
+        );
+      }
+    } catch (e) {
+      console.error(e);
+    }
+
+    return {
+      members
+    };
+  }
   render() {
     let themeColor = "#334865";
 
@@ -77,7 +128,7 @@ export default class AboutPage extends React.Component<void, OSProps, void> {
           </PageHeaderLeft>
         </PageHeader>
         <Mission />
-        <Contributors />
+        <Contributors members={this.props.members} />
       </Layout>
     );
   }
