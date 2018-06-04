53b0a956056c85de7f1959a9692da0debab6349e
Kyle Kelley <rgbkrk@gmail.com>
2018-06-03 18:53:22 -0700

    roughly type _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,30 @@
+// @flow
 import App, { Container } from 'next/app';
 import React from 'react';
 import { App as AppWrapper } from '@components/app';
 import { Header } from '@components/header';
 import { Footer } from '@components/footer';
 import { WindowSize } from 'react-fns';
+
+// Should these types come from next.js directly somehow?
+type AppContext = {
+  pathname: string,
+  query: any,
+  req?: any,
+  res?: any,
+  jsonPageRes?: any,
+  err?: any,
+};
+type AppProps<P> = {
+  Component: React.Component<*, *> & {
+    getInitialProps: ?(ctx: AppContext) => P,
+  },
+  router: *,
+  ctx: AppContext,
+};
+
 class MyApp extends App {
-  static async getInitialProps({ Component, router, ctx }) {
+  static async getInitialProps({ Component, router, ctx }: AppProps<*>) {
     let pageProps = {};
 
     if (Component.getInitialProps) {
