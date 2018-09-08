86753edc2f9b72608fa3149293669a96f1a8ff9c
John Detlefs <jdetle@gmail.com>
2018-09-08 17:21:58 -0500

    Start page-header work

---
+++ components/page-header/styled/index.js
@@ -0,0 +1,24 @@
+import styled, { css } from 'styled-components';
+import { sif, wrapperStyles } from '@common/styled';
+
+const Left = styled.div`
+
+`
+
+const Right = styled.div `
+
+`
+
+const StyledPageHeader = styled.section`
+
+`
+
+const Wrapper = styled.div`
+
+`
+
+StyledPageHeader.Left = Left
+StyledPageHeader.Right = Right
+StyledPageHeader.Wrapper = Wrapper 
+
+export { StyledPageHeader }
