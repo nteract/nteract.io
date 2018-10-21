8794674ac0ddc1b7d5cb81754fba006dc62f11c3
Lukas Geiger <lukas.geiger94@gmail.com>
2018-10-22 00:46:16 +0200

    Remove old post-css based setup

--- next.config.js
+++ next.config.js
@@ -1,39 +1,8 @@
-const path = require('path');
-const glob = require('glob');
 const webpack = require('./webpack.config');
+
 module.exports = {
   webpack: (config, { dev }) => {
     config.resolve = webpack.resolve;
-    config.module.rules.push(
-      {
-        test: /\.(css|scss)/,
-        loader: 'emit-file-loader',
-        options: {
-          name: 'dist/[path][name].[ext]',
-        },
-      },
-      {
-        test: /\.css$/,
-        use: ['babel-loader', 'raw-loader', 'postcss-loader'],
-      },
-      {
-        test: /\.s(a|c)ss$/,
-        use: [
-          'babel-loader',
-          'raw-loader',
-          'postcss-loader',
-          {
-            loader: 'sass-loader',
-            options: {
-              includePaths: ['styles', 'node_modules']
-                .map((d) => path.join(__dirname, d))
-                .map((g) => glob.sync(g))
-                .reduce((a, c) => a.concat(c), []),
-            },
-          },
-        ],
-      },
-    );
     return config;
   },
 };
