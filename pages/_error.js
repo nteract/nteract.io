db802fa7044905fdeea69bdb2a648e6c5ef1ba95
Kyle Kelley <rgbkrk@gmail.com>
2018-10-28 12:01:21 -0700

    No more redirecting to nteract.github.io
    Closes #84.

--- pages/_error.js
+++ pages/_error.js
@@ -7,26 +7,20 @@ export default class Error extends React.Component<*, *> {
     return { statusCode, pathname };
   }
 
-  componentDidMount() {
-    // We redirect to nteract.github.io to handle our /docs pages and others that
-    // are still served on GitHub pages
-
-    // In the future we can make a better custom error page here
-    if (this.props.statusCode === 404) {
-      window.location.replace(
-        `https://nteract.github.io${this.props.pathname}`
-      );
-    }
-  }
-
   render() {
     if (this.props.statusCode === 404) {
       return (
-        <p>
-          {`${
-            this.props.pathname
-          } not on nteract.io, redirecting to nteract.github.io`}
-        </p>
+        <>
+          <h1>404 😨</h1>
+          <p>
+            {`${this.props.pathname} not on nteract.io. Raise an issue on `}
+            <a href="https://github.com/nteract/nteract.io/issues">
+              the nteract.io github repository
+            </a>
+            {`. For now though, you can make your way `}
+            <a href="/">Home</a>
+          </p>
+        </>
       );
     } else if (this.props.statusCode) {
       return <p>{`An error ${this.props.statusCode} occurred on server`}</p>;
