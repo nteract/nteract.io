00a6a5ddc597af781969b6394c23b5bff20bd455
John Detlefs <jdetle@gmail.com>
2018-10-31 15:40:13 -0500

    Fix error page.

--- pages/_error.js
+++ pages/_error.js
@@ -1,31 +1,47 @@
 // @flow
 
 import * as React from "react";
+import Layout from "@components/layout";
+import { ContentSection } from "@components/content-section";
+import { Type } from "@components/typography";
+import { PageHeader } from "@components/page-header";
 export default class Error extends React.Component<*, *> {
   static getInitialProps({ req, res, xhr, pathname }: *) {
     const statusCode = res ? res.statusCode : xhr ? xhr.status : null;
     return { statusCode, pathname };
   }
-
+  
   render() {
-    if (this.props.statusCode === 404) {
-      return (
-        <>
-          <h1>404 😨</h1>
-          <p>
+    let message;
+    console.log(this.props.statusCode)
+    switch(this.props.statusCode) {
+      case undefined:
+        message = <Type.p>Unknown error, no status code to report.</Type.p>
+        break;
+      case 404:
+        message = (<>
+          <Type.h1 style={{ fontSize: "70px", fontWeight: "900", margin: 0 }}>404 😨</Type.h1>
+          <ContentSection.Pane style={{margin: 0}}>
             {`${this.props.pathname} not on nteract.io. Raise an issue on `}
             <a href="https://github.com/nteract/nteract.io/issues">
               the nteract.io github repository
             </a>
             {`. For now though, you can make your way `}
             <a href="/">Home</a>
-          </p>
-        </>
+          </ContentSection.Pane>
+        </>)
+        break;
+      default:
+        message = (<Type.p>{`An error ${this.props.statusCode}`}</Type.p>)
+        break;
+    }
+      return (
+        <Layout>
+          <PageHeader themeColor="#334865"/>
+          <ContentSection center>
+            {message}
+          </ContentSection>
+        </Layout>
       );
-    } else if (this.props.statusCode) {
-      return <p>{`An error ${this.props.statusCode} occurred on server`}</p>;
-    } else {
-      return <p>Unknown error :(</p>;
     }
   }
-}
