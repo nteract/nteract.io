f52f49e525b58360557af13181fabce8646ea0df
Carol Willing <carolcode@willingconsulting.com>
2018-12-13 09:12:00 -0800

    clean up comment indents

--- components/kernels/python.js
+++ components/kernels/python.js
@@ -6,28 +6,33 @@ import SyntaxHighlighter from "react-syntax-highlighter";
 import { github } from "react-syntax-highlighter/styles/hljs";
 import { ContentSection } from "../content-section";
 
-const pipInstall = `python3 -m venv my_environment_name  # create a virtual environment
+const pipInstall = `
+python3 -m venv my_environment_name      # create a virtual environment
 source my_environment_name/bin/activate  # activate the virtual environment
-python -m pip install ipykernel  # install the python kernel (ipykernel) into the virtual environment
-python -m ipykernel install  # install python kernel into nteract's available kernel list`;
+python -m pip install ipykernel          # install the python kernel (ipykernel) into the virtual environment
+python -m ipykernel install              # install python kernel into nteract's available kernel list`;
 
-const pip2Install = `python -m pip install virtualenv  # install a package for creating virtual environment
-cd my_project_folder  # create a project folder
-virtualenv my_project  # create a virtual environment
-source my_project/bin/activate  # activate the virtual environment
-python -m pip install ipykernel  # install the python kernel (ipykernel) into the virtual environment
-python -m ipykernel install # install python kernel into nteract's available kernel list`;
+const pip2Install = `
+python -m pip install virtualenv  # install a package for creating virtual environment
+cd my_project_folder              # create a project folder
+virtualenv my_project             # create a virtual environment
+source my_project/bin/activate    # activate the virtual environment
+python -m pip install ipykernel   # install the python kernel (ipykernel) into the virtual environment
+python -m ipykernel install       # install python kernel into nteract's available kernel list`;
 
-const pipGlobalInstall = `python3 -m pip install ipykernel  # install the python kernel (ipykernel) globally
-python3 -m ipykernel install  # install python kernel into nteract's available kernel list`;
+const pipGlobalInstall = `
+python3 -m pip install ipykernel  # install the python kernel (ipykernel) globally
+python3 -m ipykernel install      # install python kernel into nteract's available kernel list`;
 
-const pip2GlobalInstall = `python -m pip install ipykernel  # install the python kernel (ipykernel) globally
-python -m ipykernel install # install python kernel into nteract's available kernel list`;
+const pip2GlobalInstall = `
+python -m pip install ipykernel   # install the python kernel (ipykernel) globally
+python -m ipykernel install       # install python kernel into nteract's available kernel list`;
 
-const condaInstall = `conda create -n my_conda python=3   # Or python=2 for Python 2
-source activate my_conda    # On Windows, remove the word 'source'
-conda install ipykernel  # install Python kernel (ipykernel) into the conda environment
-python -m ipykernel install`;
+const condaInstall = `
+conda create -n my_conda python=3  # Or python=2 for Python 2
+source activate my_conda           # On Windows, remove the word 'source'
+conda install ipykernel            # install Python kernel (ipykernel) into the conda environment
+python -m ipykernel install        # install python kernel into nteract's available kernel list`;
 
 export default () => (
   <ContentSection>
