882246d28253df369b0dae6301eee7ddfaf0f77b
Carol Willing <carolcode@willingconsulting.com>
2018-12-18 15:00:35 -0800

    update julia link and install

--- components/kernels/julia.js
+++ components/kernels/julia.js
@@ -5,7 +5,10 @@ import SyntaxHighlighter from "react-syntax-highlighter";
 import { github } from "react-syntax-highlighter/styles/hljs";
 import { ContentSection } from "../content-section";
 
-const install = `Pkg.add("IJulia")`;
+const install = `
+# From a Julia prompt
+using Pkg
+Pkg.add("IJulia")`;
 
 export default () => (
   <ContentSection>
@@ -13,12 +16,15 @@ export default () => (
       <Kernel
         displayName="Julia"
         repository="https://github.com/JuliaLang/IJulia.jl"
-        installURL="https://irkernel.github.io/installation/"
+        installURL="https://github.com/JuliaLang/IJulia.jl#installation"
         logo="https://raw.githubusercontent.com/JuliaLang/IJulia.jl/master/deps/ijulialogo.png"
       >
         <h3>Installation</h3>
         <div className="columns">
           <div className="column">
+            <p>
+              Install Julia to your system. https://julialang.org/downloads/
+            </p>
             <h4>Within Julia</h4>
             <SyntaxHighlighter language="julia" style={github}>
               {install}
