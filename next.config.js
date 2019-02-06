e6d81c0fe2d8fed3f8bb65df929b9ad9c7003c74
Kyle Kelley <rgbkrk@gmail.com>
2019-02-06 09:25:39 -0800

    put the webpack config back in

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const webpack = require("./webpack.config");
 
 module.exports = {
+  webpack: (config, { dev }) => {
+    config.resolve = webpack.resolve;
+    return config;
+  },
   target: "serverless",
   exportPathMap: async function(defaultPathMap) {
     return {
